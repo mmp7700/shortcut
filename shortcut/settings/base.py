ecc87b8d73b206725ee3e9b07dff9a8ba9904cb6
mmp7700 <michael@itsborderline.com>
2018-04-03 16:32:35 -0400

    prepped for heroku

--- shortcut/settings/base.py
+++ shortcut/settings/base.py
@@ -78,16 +78,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'shortcut.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -135,10 +125,9 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 
 # Wagtail settings
--- shortcut/settings/dev.py
+++ shortcut/settings/dev.py
@@ -9,6 +9,14 @@ SECRET_KEY = 'l+sry*&gci3szu4o1)_p)l#bn!pn6rr=28n+a=4(v7q_sb&zuk'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_URL = '/static/'
 
 try:
     from .local import *
