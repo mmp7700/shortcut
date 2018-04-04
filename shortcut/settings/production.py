2263234d13c55e5f6a8d9ddc4ee19a19cffa9bfc
mmp7700 <michael@itsborderline.com>
2018-04-04 08:50:12 -0400

    moved aws key to env variable

--- shortcut/settings/production.py
+++ shortcut/settings/production.py
@@ -11,9 +11,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 DATABASES = {'default': dj_database_url.config()}
 DATABASES['default']['CONN_MAX_AGE'] = 500
 
-AWS_STORAGE_BUCKET_NAME = 'shortcutdigital'
-AWS_ACCESS_KEY_ID = 'AKIAIUXYGURJFOHWP73Q'
-AWS_SECRET_ACCESS_KEY = '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'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
