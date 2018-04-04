bf24681b7524bb053e22798572e4c4e7c5110ce6
mmp7700 <michael@itsborderline.com>
2018-04-04 09:14:53 -0400

    removed keys

--- shortcut/settings/production.py
+++ shortcut/settings/production.py
@@ -11,9 +11,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 DATABASES = {'default': dj_database_url.config()}
 DATABASES['default']['CONN_MAX_AGE'] = 500
 
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % ['AWS_STORAGE_BUCKET_NAME']
-
-MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+MEDIA_URL = "https://%s/" % ['AWS_S3_CUSTOM_DOMAIN']
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
